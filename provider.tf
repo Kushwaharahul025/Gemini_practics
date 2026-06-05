98f3a268c67858b6d52190db6cd5570a7c47332b
kushwaharahul025 <kushwaharahu025@gmail.com>
2026-06-05 09:48:52 +0530

    Fix pipeline setup and configure remote backend

--- provider.tf
+++ provider.tf
@@ -2,11 +2,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "4.32.0"
     }
   }
+  backend "azurerm" {
+
+  resource_group_name = "rahulrg"
+  storage_account_name = "rahulstg123"
+  container_name = "hello"
+  key = "new.terraform.tfstate"
+}
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = "31b619bb-eedc-49a3-a81b-06670c4e81fe"
 }
