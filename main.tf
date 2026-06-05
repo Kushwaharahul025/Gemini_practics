1175356d3b34151a7f4472cbcb1563647106b885
kushwaharahul025 <kushwaharahu025@gmail.com>
2026-06-05 09:13:40 +0530

    Rename Resource Group to rahul5 and Storage Account to rohit12

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "rahul5"
   location = "West Europe"
 }
 
 resource "azurerm_storage_account" "example" {
-  name                     = "examplestoracc12345"
+  name                     = "rohit12"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
